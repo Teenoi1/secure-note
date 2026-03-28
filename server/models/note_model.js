89b9f06ab403c39e08fda060faaeb301538708a2
Teenoi1 <chayakornjaikanya@gmail.com>
2026-03-28 19:34:23 +0700

    JSON Model for Notes

---
+++ server/models/note_model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+// Note Schema
+const noteSchema = new mongoose.Schema({
+    content: {
+      type: String,
+      required: true,
+      maxlength: 100000,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+},
+    {
+        versionKey: false,
+    }
+);
+
+export const Note = mongoose.model("Note", noteSchema);
